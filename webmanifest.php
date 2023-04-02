94bbba9b317f31c78739900a3caa944ae1867e4c
for3um <129652562+for3um@users.noreply.github.com>
2023-04-02 13:53:59 +0300

    Add files via upload

---
+++ webmanifest.php
@@ -0,0 +1,11 @@
+{
+    "name": "ONLYRP MTA | \u0424\u043e\u0440\u0443\u043c",
+    "short_name": "ONLYRP | MTA",
+    "lang": "ru-RU",
+    "dir": "LTR",
+    "display": "minimal-ui",
+    "scope": "\/",
+    "start_url": "\/?_pwa=1",
+    "background_color": "#171e29",
+    "theme_color": "#0da9ef"
+}
